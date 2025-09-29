6e3da3a470ec6bd1929ef60c072d4122c8a163dd
Henry-346 <archerhenr@myvuw.ac.nz>
2025-09-29 14:14:12 +1300

    -

---
+++ music_dayone.js
@@ -0,0 +1,10 @@
+
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+//function draw_one_frame(words, vocal, drum, bass, other, counter) {
+//  background(20)
+// let sunY = 350;
+// let sunSize; 
+// sunSize = map(bass, 10 , 100,10,500)
+// fill(250,0,200)// pink
+// ellipse(300,sunY,sunSize)
+//}
--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,67 @@
-
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+
+   //Slider bar 
+var vocalMap =map(vocal, 0,10,30,90);
+var lengthOfLine = 140;
+var lineStart = 525;
+var lineEnd = lineStart+lengthOfLine;
+
+ // disc
+ background(20)
+ let sunY = 350;
+ let sunX = 250;
+ let sunSize; 
+ sunSize = map(drum, 10 , 100,200,500);
+
+ // disc 
+ fill(50)// grey
+ ellipse(sunX,sunY,sunSize);
+
+   //right side of screen 
+   fill(50);
+   rect(500,0,1000,720);
+
+      //black rectangle 
+      fill(10);
+      rect(520,10,150,690);
+      
+        //grey rectangle
+        fill(100);
+        rect(690,530,570,170);
+
+           //black rectangle inside of grey one
+           fill(10);
+           //rect(700,540,550,150);
+
+//right rectangle rainbow 
+let rectSize;
+rectSize =map(bass, 20 ,100,100,500);
+fill(10);
+rect(700,540,550,150);
+
+// squares on the right 
+fill(100);
+square(730,20,150,10)
+square(900,20,150,10)
+square(1070,20,150,10)
+
+square(730,190,150,10)
+square(900,190,150,10)
+square(1070,190,150,10)
  
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+square(730,360,150,10)
+square(900,360,150,10)
+square(1070,360,150,10)
+
+
+
+colorMode(HSB, 100);
+strokeWeight(5);
+stroke(drum,80,80);
+for(var i =1; i <= vocalMap; i++){
+  var lineStep = i+10;
+  line(lineStart, lineStep, lineEnd, lineStep);
+  }
+
 }
+//
